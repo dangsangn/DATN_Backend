4fbd36fd101a2179ec3d49d6b3f37e03c975a21e
daitd <dangsangnguyen260399@gmail.com>
2021-12-24 00:23:40 +0700

    add ip

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const express = require("express");
+const express = require("express"),
+  http = require("http"),
+  app = express(),
+  server = http.createServer(app);
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -14,7 +17,6 @@ const provinceRouter = require("./routers/province");
 const upload = require("./multer");
 const cloudinary = require("./cloudinary");
 
-const app = express();
 dotenv.config();
 const corsConfig = {
   credentials: true,
@@ -69,22 +71,12 @@ app.use("/api/upload-images", upload.array("image"), async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log("listening on port ", PORT);
+// app.listen(PORT, () => {
+//   console.log("listening on port ", PORT);
+// });
+
+server.listen(8080, "127.0.0.1", function () {
+  server.close(function () {
+    server.listen(8080, "0.0.0.0");
+  });
 });
-
-/////////////////////////////////
-var http = require("http");
-var parse = require("url").parse;
-var handleRequest = function (req, res) {
-  const defaultValue = "world";
-  var query = parse(req.url, true).query;
-  var name = query.name || defaultValue;
-
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end(`Hello ${name}\n`);
-};
-
-http.createServer(handleRequest).listen(8080, "0.0.0.0");
-
-console.log("Server running at http://0.0.0.0:8080/");
