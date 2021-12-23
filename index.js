513ab91971edc11beb33c2c1e503e053c076c413
daitd <dangsangnguyen260399@gmail.com>
2021-12-24 00:11:48 +0700

    add html

--- index.js
+++ index.js
@@ -74,17 +74,17 @@ app.listen(PORT, () => {
 });
 
 /////////////////////////////////
-// var http = require("http");
-// var parse = require("url").parse;
-// var handleRequest = function (req, res) {
-//   const defaultValue = "world";
-//   var query = parse(req.url, true).query;
-//   var name = query.name || defaultValue;
+var http = require("http");
+var parse = require("url").parse;
+var handleRequest = function (req, res) {
+  const defaultValue = "world";
+  var query = parse(req.url, true).query;
+  var name = query.name || defaultValue;
 
-//   res.writeHead(200, { "Content-Type": "text/plain" });
-//   res.end(`Hello ${name}\n`);
-// };
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(`Hello ${name}\n`);
+};
 
-// http.createServer(handleRequest).listen(8080, "0.0.0.0");
+http.createServer(handleRequest).listen(8080, "0.0.0.0");
 
-// console.log("Server running at http://0.0.0.0:8080/");
+console.log("Server running at http://0.0.0.0:8080/");
