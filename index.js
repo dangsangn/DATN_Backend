518c9850453f818774f404107cbd17d485a4b245
daitd <dangsangnguyen260399@gmail.com>
2021-12-26 14:49:16 +0700

    dix origin cors

--- index.js
+++ index.js
@@ -76,23 +76,3 @@ app.listen(PORT,function () {
   console.log("Server running at ", PORT);
 });
 
-// server.listen(8080, "127.0.0.1", function () {
-//   server.close(function () {
-//     server.listen(8080, "0.0.0.0");
-//   });
-// });
-
-// var http = require("http");
-// var parse = require("url").parse;
-// var handleRequest = function (req, res) {
-//   const defaultValue = "world";
-//   var query = parse(req.url, true).query;
-//   var name = query.name || defaultValue;
-
-//   res.writeHead(200, { "Content-Type": "text/plain" });
-//   res.end(`Hello ${name}\n`);
-// };
-
-// http.createServer(handleRequest).listen(8080, "0.0.0.0");
-
-// console.log("Server running at http://0.0.0.0:8080/");
